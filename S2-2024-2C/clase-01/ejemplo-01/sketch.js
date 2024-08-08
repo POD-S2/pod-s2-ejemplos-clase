a4e969234e907ffa9c9d681b9acbd8806cecb4e5
nayesz <69222696+Nayesz@users.noreply.github.com>
2024-08-07 21:21:12 -0300

    agrego ejemplos

---
+++ S2-2024-2C/clase-01/ejemplo-01/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 500);
+}
+
+function draw() {
+  background(220);
+  //point(200,200);
+  //Ahora voy a dibujar una linea:
+  //line(1,3,200,500)
+  //triangle(158,55,290,91,290,400);
+  rect(2,3,50,40)
+
+  
+}
---
+++ S2-2024-2C/clase-01/ejemplo-02/tp01_nayla_zarate/punto_a/sketch.js
@@ -0,0 +1,23 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(100)
+  rect(100,100,10,40)
+  rect(110,90,10,60)
+  rect(120,90,10,70)
+  rect(130,90,10,80)
+  rect(140,100,10,80)
+  // Fin de primera mitad de corazon
+  rect(150,110,10,80)
+
+  rect(160,100,10,80)
+  rect(170,90,10,80)
+  rect(180,90,10,70)
+  rect(190,90,10,60)
+  rect(200,100,10,40)
+
+
+
+}
